260ce4f4586b0eda24d8eebbfd2609aab7089a06
Dosssik <dosssik@ya.ru>
2017-07-25 21:53:21 +0300

    first comments

--- app/src/main/java/com/fed/FlickrGal/GalleryFragment.java
+++ app/src/main/java/com/fed/FlickrGal/GalleryFragment.java
@@ -20,7 +20,9 @@ import java.util.List;
 
 public class GalleryFragment extends Fragment {
 
-    public static final String TAG = "GalleryFragment";
+    public static final String TAG = "GalleryFragment";     // instead create static variable in each fragment, u can
+                                                            // use YourFragment.class.getSimpleName() as tag.
+                                                            // when u'll have over 20 fragments in app - u'll be tied to create special tag for each one
 
     private RecyclerView mPhotoRecyclerView;
     private List<GalleryItem> mItems = new ArrayList<>();
@@ -32,8 +34,10 @@ public class GalleryFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRetainInstance(true);
-        new FetchItemTask().execute();
+        setRetainInstance(true);        // Take care with that. If it only for avoid recreation of fragment - better to find another solution
+
+        new FetchItemTask().execute();  // it will fire every rotation - better way is to fetch data and persist somewhere.
+                                        // when device'll rotated - your can set already downloaded items
     }
 
     @Nullable
@@ -51,20 +55,7 @@ public class GalleryFragment extends Fragment {
         if (isAdded()) mPhotoRecyclerView.setAdapter(new PhotoAdapter(mItems));
     }
 
-    private class PhotoHolder extends RecyclerView.ViewHolder {
-        private TextView mTitleTextView;
-
-        public PhotoHolder(View itemView) {
-            super(itemView);
-            mTitleTextView = (TextView) itemView;
-        }
-
-        public void bindGalleryItem(GalleryItem item) {
-            mTitleTextView.setText(item.toString());
-        }
-    }
-
-    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {      // Should be in separated class
 
         private List<GalleryItem> mGalleryItems;
 
@@ -90,6 +81,21 @@ public class GalleryFragment extends Fragment {
         }
     }
 
+    private class PhotoHolder extends RecyclerView.ViewHolder {     // Usually holder is private static class in adapter. Not critical, just FYI
+        private TextView mTitleTextView;
+
+        public PhotoHolder(View itemView) {
+            super(itemView);
+            mTitleTextView = (TextView) itemView;
+        }
+
+        public void bindGalleryItem(GalleryItem item) {
+            mTitleTextView.setText(item.toString());
+        }
+    }
+
+    // TODO: When you'll finish Retrofit & Dagger2 learning - remove AsyncTask, and replace it by better solution.
+    // as on of popular interview question, be ready to answer "What's wrong with AsyncTask? Why we shouldn't use it?"
     private class FetchItemTask extends AsyncTask<Void, Void, List<GalleryItem>> {
         @Override
         protected List<GalleryItem> doInBackground(Void... params) {
