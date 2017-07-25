d03ca3d79a020f1fd9e0f89e9c47d74d411c3707
surinfv <surinfv@gmail.com>
2017-07-25 16:01:38 +0300

    added Recycler. load text from Image info to Recycler Item.

--- app/src/main/java/com/fed/FlickrGal/GalleryFragment.java
+++ app/src/main/java/com/fed/FlickrGal/GalleryFragment.java
@@ -9,6 +9,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by f on 24.07.2017.
@@ -17,9 +21,11 @@ import android.view.ViewGroup;
 public class GalleryFragment extends Fragment {
 
     public static final String TAG = "GalleryFragment";
-    private RecyclerView mPhotoRecycler;
 
-    public static GalleryFragment newInstance() {
+    private RecyclerView mPhotoRecyclerView;
+    private List<GalleryItem> mItems = new ArrayList<>();
+
+    public static GalleryFragment newInstance(){
         return new GalleryFragment();
     }
 
@@ -32,21 +38,68 @@ public class GalleryFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_gallery, container, false);
 
-        mPhotoRecycler = v.findViewById(R.id.fragment_gallery_recycler);
-        mPhotoRecycler.setLayoutManager(new GridLayoutManager(getActivity(), 2));
-
+        mPhotoRecyclerView = v.findViewById(R.id.fragment_gallery_recycler);
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        setupAdapter();
         return v;
     }
 
-    private class FetchItemTask extends AsyncTask<Void, Void, Void> {
+    private void setupAdapter(){
+        if (isAdded()) mPhotoRecyclerView.setAdapter(new PhotoAdapter(mItems));
+    }
+
+    private class PhotoHolder extends RecyclerView.ViewHolder {
+        private TextView mTitleTextView;
+
+        public PhotoHolder(View itemView) {
+            super(itemView);
+            mTitleTextView = (TextView) itemView;
+        }
+
+        public void bindGalleryItem(GalleryItem item) {
+            mTitleTextView.setText(item.toString());
+        }
+    }
+
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
+
+        private List<GalleryItem> mGalleryItems;
+
+        public PhotoAdapter(List<GalleryItem> galleryItems) {
+            mGalleryItems = galleryItems;
+        }
 
         @Override
-        protected Void doInBackground(Void... params) {
-            new FlickrFetcher().fetchItems();
-            return null;
+        public PhotoHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            TextView textView = new TextView(getActivity());
+            return new PhotoHolder(textView);
+        }
+
+        @Override
+        public void onBindViewHolder(PhotoHolder holder, int position) {
+            GalleryItem galleryItem = mGalleryItems.get(position);
+            holder.bindGalleryItem(galleryItem);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mGalleryItems.size();
+        }
+    }
+
+    private class FetchItemTask extends AsyncTask<Void, Void, List<GalleryItem>> {
+        @Override
+        protected List<GalleryItem> doInBackground(Void... params) {
+            return new FlickrFetcher().fetchItems();
+        }
+
+        @Override
+        protected void onPostExecute(List<GalleryItem> galleryItems) {
+            mItems = galleryItems;
+            setupAdapter();
         }
     }
 }
